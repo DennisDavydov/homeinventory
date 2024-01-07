1b87ed7eef8fa0036a90360655144a336509a97a
DennisDavydov <denisdavydov110893@gmail.com>
2024-01-07 15:45:47 +0200

    minor fixes

--- InventoryServer/app.js
+++ InventoryServer/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-const port = 3000;
+const port = 8080;
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Actimel1234',
     database: 'inventory_db'
 });
 
@@ -29,14 +29,41 @@ connection.connect((err) => {
 // Route for the home page
 app.get('/', (req, res) => {
     // Fetch data from the products table
-    const sql = 'SELECT * FROM products';
-    connection.query(sql, (error, results) => {
-        if (error) {
-            console.error('Error fetching data:', error);
-            return res.status(500).send('Error fetching data');
-        }
-        // Render the index.ejs template with the fetched data
-        res.render('index', { products: results,  });
+    const productsSQL = 'SELECT * FROM (SELECT p.*, t.total_amount, t.earliest_expiry_date, ROW_NUMBER() OVER(PARTITION BY p.barcode ORDER BY p.id) AS row_num FROM products p JOIN (SELECT barcode, SUM(amount) AS total_amount, MIN(expiry_date) AS earliest_expiry_date FROM products GROUP BY barcode) t ON p.barcode = t.barcode) subquery WHERE row_num = 1';
+
+    // Fetch data from the second table
+    const secondTableSQL = 'SELECT * FROM products';
+
+    // Use Promise.all to execute both queries in parallel
+    Promise.all([
+        new Promise((resolve, reject) => {
+            connection.query(productsSQL, (error, productsResults) => {
+                if (error) {
+                    console.error('Error fetching data from products table:', error);
+                    reject(error);
+                } else {
+                    resolve(productsResults);
+                }
+            });
+        }),
+        new Promise((resolve, reject) => {
+            connection.query(secondTableSQL, (error, secondTableResults) => {
+                if (error) {
+                    console.error('Error fetching data from second table:', error);
+                    reject(error);
+                } else {
+                    resolve(secondTableResults);
+                }
+            });
+        })
+    ])
+    .then(([products, secondTableData]) => {
+        // Render the index.ejs template with both sets of fetched data
+        res.render('index', { products: products, fullproducts: secondTableData });
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Error fetching data');
     });
 });
 
